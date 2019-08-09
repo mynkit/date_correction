c2fb5b90fa07e08291a8be26fcbc3657003833aa
keita miyano <keita.miyano@estie.co.jp>
2019-08-09 14:10:49 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# date_correction
+
+日付をフォーマットを統一する('yyyy/mm/dd'型もしくは'yyyy/mm'型にする)
+
+# install
+
+```shell
+pip install -U date_correction
+```
+
+# usage
+
+```python
+correction = date_correction()
+date_ymd = correction.jap_to_west("令和元年5月1日")
+print(date_ymd) # '2019/05/01'
+date_ym = correction.jap_to_west("令和元年5月1日", remove_day=True)
+print(date_ym) # '2019/05'
+```
+
---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+
+setup(
+    name="date_correction",
+    version="0.0.1",
+    python_requires=">=3.5",
+    author='mynkit',
+    author_email='dcm5124289@gmail.com',
+    packages=[
+        "date_correction",
+    ],
+    url="https://github.com/mynkit/date_correction",
+)
